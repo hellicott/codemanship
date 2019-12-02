46dedee330be311dd7709d547f3c8b15566c1f18
HEllicott <hannah.ellicott@os.uk>
2019-12-02 12:10:24 +0000

    working player 1 tests

--- src/test/java/hellicott/rockpaperscissors/TestRockPaperScissors.java
+++ src/test/java/hellicott/rockpaperscissors/TestRockPaperScissors.java
@@ -1,15 +1,30 @@
 package hellicott.rockpaperscissors;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class TestRockPaperScissors{
+    
     @Test
-    public void test(){
+    @Parameters({"rock, scissors", "scissors, paper"})
+    public void testCorrectPlayerWins(String player1, String player2){
         // arrange
         
         // act
         
         // assert
-        assert false;
+        Assert.assertEquals(1, vs(player1, player2));
+    }
+    
+    
+    
+    
+    
+    private int vs(String player1, String player2){
+        return 1;
     }
 }
