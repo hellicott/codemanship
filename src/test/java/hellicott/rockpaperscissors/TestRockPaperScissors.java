c911a153e36abf6d32f8a8da1cda092408661621
HEllicott <hannah.ellicott@os.uk>
2019-12-02 12:50:11 +0000

    refactored tests

--- src/test/java/hellicott/rockpaperscissors/TestRockPaperScissors.java
+++ src/test/java/hellicott/rockpaperscissors/TestRockPaperScissors.java
@@ -25,11 +25,17 @@ public class TestRockPaperScissors{
     
     @Test
     @Parameters({"1, scissors, paper", "2, paper, scissors"})
-    public void testScissorsBeatPaper(int expected, String player1, String player2){
+    public void testScissorsBeatsPaper(int expected, String player1, String player2){
         Assert.assertEquals(expected, new RockPaperScissors().vs(player1, player2));
     
     }
     
+    @Test
+    @Parameters({"1, paper, rock", "2, rock, paper"})
+    public void testPaperBeatsRock(int expected, String player1, String player2){
+        Assert.assertEquals(expected, new RockPaperScissors().vs(player1, player2));
+        
+    }
     
     
     
