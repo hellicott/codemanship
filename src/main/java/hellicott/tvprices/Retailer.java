82a4d3ed86d308c7b3cd006e860ec31cadc8d908
HEllicott <hannah.ellicott@os.uk>
2019-12-03 12:17:57 +0000

    added tv names

--- src/main/java/hellicott/tvprices/Retailer.java
+++ src/main/java/hellicott/tvprices/Retailer.java
@@ -2,4 +2,5 @@ package hellicott.tvprices;
 
 public interface Retailer{
     public double getPrice(String make, String model);
+    public String getName();
 }
--- src/test/java/hellicott/tvprices/TestCheapestPrice.java
+++ src/test/java/hellicott/tvprices/TestCheapestPrice.java
@@ -7,9 +7,9 @@ public class TestCheapestPrice{
     @Test
     public void testCanFindCheapestPriceWhenAllUnique(){
         // arrange
-        Retailer t = new RetailerStub(2.99);
-        Retailer sb = new RetailerStub(350.39);
-        Retailer atv = new RetailerStub(25.55);
+        Retailer t = new RetailerStub(2.99, "Televizion");
+        Retailer sb = new RetailerStub(350.39, "Screen Bargains");
+        Retailer atv = new RetailerStub(25.55, "Acme TV");
         TvPrices tvPrices = new TvPrices(new Retailer[]{t, sb, atv});
         // assert
         Assert.assertEquals("Televizion", TvPrices.findCheapest("make", "model"));
@@ -17,11 +17,18 @@ public class TestCheapestPrice{
     
     static class RetailerStub implements Retailer{
         private double price;
-        RetailerStub(double price){
+        private String name;
+        RetailerStub(double price, String name){
             this.price = price;
+            this.name = name;
         }
+        
         public double getPrice(String make, String model){
             return price;
+        }
+    
+        public String getName(){
+            return this.name;
         }
     }
     
